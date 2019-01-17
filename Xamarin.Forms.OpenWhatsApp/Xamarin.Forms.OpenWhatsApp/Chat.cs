844f72b5576056f2ce57e77cdbb12c07510e0b67
Juliano Custodio <juliano.custodio@hotmail.com.br>
2019-01-17 16:54:17 -0200

    Create project

---
+++ Xamarin.Forms.OpenWhatsApp/Xamarin.Forms.OpenWhatsApp/Chat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Xamarin.Forms.OpenWhatsApp
+{
+    public class Chat
+    {
+        string PhoneNumber;
+        string Message;
+
+        public Chat(string _phoneNumber, string _message = null)
+        {
+            PhoneNumber = _phoneNumber;
+            Message = _message;
+        }
+
+        public void Open()
+        {
+            try
+            {
+                var uriString = "whatsapp://send?phone=" + PhoneNumber;
+
+                if (!String.IsNullOrWhiteSpace(Message))
+                    uriString += "&text=" + Message;
+
+                Device.OpenUri(new Uri(uriString));
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
