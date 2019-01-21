6c1c2fab9d9f7eb70996cbb75ecddb27cc78d455
Juliano Custodio <juliano.custodio@hotmail.com.br>
2019-01-21 10:00:52 -0200

    Implement sample

--- OpenWhatsAppSample/OpenWhatsAppSample/OpenWhatsAppSample/MainPage.xaml.cs
+++ OpenWhatsAppSample/OpenWhatsAppSample/OpenWhatsAppSample/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.OpenWhatsApp;
 
 namespace OpenWhatsAppSample
 {
@@ -12,6 +13,18 @@ namespace OpenWhatsAppSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Send(object sender, EventArgs e)
+        {
+            try
+            {
+                Chat.Open("+5515999999999","Send this message");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
         }
     }
 }
